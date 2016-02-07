ca93b6f460a1b777926f8e6f2784be8108a42d05
Mike Tabler <mike@tabler.us>
2016-02-07 16:25:23 -0500

    Second push

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CoolLogo: UIImageView!
+    
+    @IBOutlet weak var CoolBG: UIImageView!
+    
+    @IBOutlet weak var UnCoolButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func makeMeNoSoUncool(sender: AnyObject) {
+        
+        CoolLogo.hidden = false
+        CoolBG.hidden = false
+        UnCoolButton.hidden = true
+    }
 
 }
 
